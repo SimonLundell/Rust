96ec5e501245a7615c52e829eff3359ad4d8424c
SimonLundell <simonlundell@hotmail.com>
2021-06-19 17:50:49 +0200

    Update readme. Added variable declaration, debugging and struct in main.rs

--- rust_starter_project/src/main.rs
+++ rust_starter_project/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug)]
+struct Position {
+    x:i32,
+    y:i32,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = 3; // i32 = signed 32 bit, u32 = unsigned
+    let y = &x;
+
+    let pos = Position {x:88, y:19}; // initializing position with variable values, mandatory.
+
+    println!("My position is: {}, {}", pos.y, pos.x);
+
+    println!("My debug position is: {:#?}", pos); //  This print way can only be used to debug what the values are. Requires #[derive(Debug)] , # is for pretty-print
+
+    println!("Hello {}, world!", x*y);
 }
