fc5f6b1fa1fe24ae590471e7b1698622926e9dba
Simon Lundell <simonlundell@hotmail.com>
2022-05-29 21:41:54 +0200

    no ref transpose

--- neural_networks_from_scratch/src/algebra.rs
+++ neural_networks_from_scratch/src/algebra.rs
@@ -1,19 +1,54 @@
-pub fn dot(m1: &Vec<Vec<f64>>, v2: &Vec<f64>) -> Vec<f64>
+pub fn matrix_dot(m1: &Vec<Vec<f64>>, m2: &Vec<Vec<f64>>) -> Vec<Vec<f64>>
 {
-    let mut layer_outputs = vec![0.0 as f64; 3];
-    
-    for i in 0..layer_outputs.len()
+    let mut ret = vec![];
+
+    for i in 0..m1.len()
     {
-        for j in 0..v2.len()
+        let mut row_result = vec![];
+        let temp_v1 = extract_row(&m1,i);
+        for j in 0..m2[0].len()
         {
-            layer_outputs[i] += v2[j] * m1[i][j];
+            let temp_v2 = extract_column(m2, j);
+            let dot_prod = vector_dot(&temp_v1, &temp_v2);
+            row_result.push(dot_prod);
         }
+        ret.push(row_result);
+    }
+
+    return ret;
+}
+
+
+pub fn matrix_transpose(m1: Vec<Vec<f64>>) -> Vec<Vec<f64>>
+{
+    let mut ret = vec![];
+
+    for i in 0..m1[0].len()
+    {
+        let mut temp_v = vec![];
+        for j in 0..m1.len()
+        {
+            temp_v.push(m1[j][i]);
+        }
+        ret.push(temp_v);
+    }
+
+    return ret;
+}
+
+pub fn vector_dot(v1: &Vec<f64>, v2: &Vec<f64>) -> f64
+{
+    let mut layer_outputs = 0.0;
+    
+    for i in 0..v1.len()
+    {
+        layer_outputs += v1[i]*v2[i];
     }
 
     return layer_outputs;
 }
 
-pub fn add(v1: &Vec<f64>, v2: &Vec<f64>) -> Vec<f64>
+pub fn vector_add(v1: &Vec<f64>, v2: &Vec<f64>) -> Vec<f64>
 {
     let mut ret = vec![0.0 as f64; v1.len()];
     for i in 0..ret.len()
@@ -22,4 +57,19 @@ pub fn add(v1: &Vec<f64>, v2: &Vec<f64>) -> Vec<f64>
     }
 
     return ret;
+}
+
+pub fn extract_row(m: &Vec<Vec<f64>>, idx: usize) -> Vec<f64>
+{
+    return m[idx].clone();
+}
+
+pub fn extract_column(m: &Vec<Vec<f64>>, idx: usize) -> Vec<f64>
+{
+    let mut ret_v = vec![];
+    for i in 0..m.len()
+    {
+        ret_v.push(m[i][idx]);
+    }
+    return ret_v;
 }
--- neural_networks_from_scratch/src/main.rs
+++ neural_networks_from_scratch/src/main.rs
@@ -2,22 +2,28 @@ mod algebra;
 
 fn main() {
     // Sample first neuron
-    let inputs = vec![1.0 as f64, 2.0, 3.0, 2.5]; // Input from a neuron in a layer earlier
+    let inputs = vec![vec![1.0 as f64, 2.0, 3.0, 2.5],
+                        vec![2.0 as f64, 5.0, -1.0, 2.0],
+                        vec![-1.5 as f64, 2.7, 3.3, -0.8]]; // Input from a neuron in a layer earlier
 
     // Each input has an associated unique weight
-    let weights = vec![vec![0.2 as f64, 0.8, -0.5, 1.0], 
+    let mut weights = vec![vec![0.2 as f64, 0.8, -0.5, 1.0], 
                     vec![0.5 as f64, -0.91, 0.26, -0.5], 
                     vec![-0.26 as f64, -0.27, 0.17, 0.87]];
+
+    weights = algebra::matrix_transpose(weights);
 
     // Each neuron has a unique bias, offsets the value in a neuron
     let biases = vec![2.0 as f64, 3.0, 0.5];
 
     // Perform dot product, important to have weights first
-    let mut outputs = algebra::dot(&weights, &inputs);
+    let mut outputs = algebra::matrix_dot(&inputs, &weights);
 
     // Perform list addition (element by element addition)
-    outputs = algebra::add(&outputs, &biases);
-
+    for i in 0..outputs.len()
+    {
+        outputs[i] = algebra::vector_add(&outputs[i], &biases);
+    }
 
     println!("{:?}", outputs);
 }
