32d28f719d18d322d0a100df78800e406ea88e42
Simon Lundell <simonlundell@hotmail.com>
2022-05-31 21:57:54 +0200

    Second layer

--- neural_networks_from_scratch/src/main.rs
+++ neural_networks_from_scratch/src/main.rs
@@ -10,20 +10,33 @@ fn main() {
     let mut weights = vec![vec![0.2 as f64, 0.8, -0.5, 1.0], 
                     vec![0.5 as f64, -0.91, 0.26, -0.5], 
                     vec![-0.26 as f64, -0.27, 0.17, 0.87]];
-
+    // Second layer
+    let mut weights2 = vec![vec![0.1 as f64, -0.14, 0.5], 
+                    vec![-0.5 as f64, 0.12, -0.33,], 
+                    vec![-0.44 as f64, 0.73, -0.13]];
+    
     weights = algebra::matrix_transpose(weights);
-
+    weights2 = algebra::matrix_transpose(weights2);
     // Each neuron has a unique bias, offsets the value in a neuron
     let biases = vec![2.0 as f64, 3.0, 0.5];
+    let biases2 = vec![-1.0 as f64, 2.0, -0.5];
 
     // Perform dot product, important to have weights first
-    let mut outputs = algebra::matrix_dot(&inputs, &weights);
+    let mut layer1_outputs = algebra::matrix_dot(&inputs, &weights);
 
     // Perform list addition (element by element addition)
-    for i in 0..outputs.len()
+    for i in 0..layer1_outputs.len()
     {
-        outputs[i] = algebra::vector_add(&outputs[i], &biases);
+        layer1_outputs[i] = algebra::vector_add(&layer1_outputs[i], &biases);
     }
 
-    println!("{:?}", outputs);
+    // Second layer with first layer as input
+    let mut layer2_outputs = algebra::matrix_dot(&layer1_outputs, &weights2);
+    
+    for i in 0..layer2_outputs.len()
+    {
+        layer2_outputs[i] = algebra::vector_add(&layer2_outputs[i], &biases2);
+    }
+
+    println!("{:?}", layer2_outputs);
 }
