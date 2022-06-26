1e997dec5b9f47f1a3c58fdf068f56fc1ad05471
Simon Lundell <simonlundell@hotmail.com>
2022-06-26 11:51:56 +0200

    Loss calculation, categorical crossentropy

---
+++ neural_networks_from_scratch/src/loss_categorical_crossentropy.rs
@@ -0,0 +1,53 @@
+pub struct LossCategoricalCrossentropy
+{
+    pub loss: f64
+}
+
+impl LossCategoricalCrossentropy
+{
+    pub fn calculate(output: Vec<Vec<f64>>, y: Vec<i8>) -> LossCategoricalCrossentropy
+    {
+        let sample_losses: Vec<f64> = LossCategoricalCrossentropy::forward(output, y);
+        
+        let sum: f64 = sample_losses.iter().sum();
+        let data_loss: f64 = sum / sample_losses.len() as f64;
+
+        return LossCategoricalCrossentropy{loss: data_loss};
+    }
+
+    fn forward(y_pred: Vec<Vec<f64>>, y_true: Vec<i8>) -> Vec<f64>
+    {
+        let e = std::f64::consts::E;
+        let samples = y_pred.len();
+        let y_pred_clipped = LossCategoricalCrossentropy::clip(y_pred, 1.0*e.powf(-7.0));
+        let mut correct_confidences = vec![0.0 as f64; samples];
+
+        // Needs logic if y_true is onehot encoded
+        for i in 0..samples
+        {
+            correct_confidences[i] = -y_pred_clipped[i][y_true[i] as usize].ln();
+        }
+
+        return correct_confidences;
+    }
+
+    fn clip(mut matrix: Vec<Vec<f64>>,value: f64) -> Vec<Vec<f64>>
+    {
+        for i in 0..matrix.len()
+        {
+            for j in 0..matrix[i].len()
+            {
+                if matrix[i][j] < value
+                {
+                    matrix[i][j] = value;
+                }
+                else if matrix[i][j] > 1.0 - value
+                {
+                    matrix[i][j] = 1.0 - value;
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
--- neural_networks_from_scratch/src/main.rs
+++ neural_networks_from_scratch/src/main.rs
@@ -2,6 +2,7 @@ mod linalg;
 mod layer_dense;
 mod activation_softmax;
 mod activation_relu;
+mod loss_categorical_crossentropy;
 mod spiral_dataset;
 
 fn main() {
@@ -18,5 +19,32 @@ fn main() {
     dense2.forward(&activation1.outputs);
     let activation2: activation_softmax::ActivationSoftmax = activation_softmax::ActivationSoftmax::forward(dense2.outputs);
 
-    println!("{:?}", activation2.outputs[1]); // 300 outputs from activation 2. We will get a "perfect" 1/3 distribution per neuron prediction of the input 2 values.
+    println!("{:?}", activation2.outputs.len()); // 300 outputs from activation 2. We will get a "perfect" 1/3 distribution per neuron prediction of the input 2 values.
+
+    let loss: loss_categorical_crossentropy::LossCategoricalCrossentropy = loss_categorical_crossentropy::LossCategoricalCrossentropy::calculate(activation2.outputs, dataset.get_y());
+    println!("{:?}", loss.loss);
+
+    // First loss calculation
+    // let test_output = vec![0.7 as f64, 0.1, 0.2];
+    // let target_output = vec![1.0 as f64, 0.0, 0.0];
+
+    // // As prediction confidence gets lower, loss gets higher!
+    // let loss = -(test_output[0].ln()); // * target_output[0] + test_output[1].ln() * target_output[1] + test_output[2].ln() * target_output[2]); Can be abstracted away due to 0 and 1 multiplication
+    // println!("{}", loss); 
+
+    // let softmax_outputs = vec![vec![0.7 as f64, 0.1, 0.2],
+    //                             vec![0.1 as f64, 0.5, 0.4],
+    //                             vec![0.02 as f64, 0.9, 0.08]];
+
+    // let class_targets = vec![0, 1, 1];
+    // let mut losses = vec![0.0 as f64; class_targets.len()];
+    // for i in 0..class_targets.len()
+    // {
+    //     losses[i] = -softmax_outputs[i][class_targets[i]].ln();
+    // }
+    // println!("{:?}",losses);
+
+    // let sum: f64 = losses.iter().sum();
+    // let mean: f64 = sum / losses.len() as f64;
+    // println!("{:?}", mean)
 }
