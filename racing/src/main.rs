1bac89af7112f036559a6b5001e2dac89bc9d223
Simon Lundell <simonlundell@hotmail.com>
2023-01-28 13:17:40 +0100

    render rectangle can move

--- racing/src/main.rs
+++ racing/src/main.rs
@@ -1,34 +1,75 @@
 use ggez::event;
 use ggez::input::keyboard::{KeyCode};
-use ggez::graphics::{self, Canvas};
+use ggez::graphics::{self, Canvas, DrawParam, Color, Quad, Rect, Mesh};
 use ggez::{Context, GameResult};
 use ggez::conf::*;
-use mint;
+use mint::{Point2};
 use rand::Rng;
 
 const WINDOW_W: f32 = 600.0;
 const WINDOW_H: f32 = 800.0;
+const CAR_W: f32 = 30.0;
+const CAR_H: f32 = 60.0;
+const START_POS: Point2<f32> = Point2{x: WINDOW_W / 2.0, y: WINDOW_H - (2.0 * CAR_H)};
 
-struct MainState {}
+struct Car {
+    w: f32,
+    h: f32,
+    pos: Point2<f32>,
+    // ref_p: Point2<f32>,
+}
+
+impl Car {
+    fn new(w: f32, h: f32, pos: Point2<f32>) -> Car {
+        let car = Car{w: w, h: h, pos: pos};
+        car
+    }
+
+    fn draw(&mut self, canvas: &mut Canvas, ctx: &mut Context) -> GameResult {
+        let rect = Rect::new(self.pos.x, self.pos.y, self.w, self.h);
+        let rect_mesh = Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(4.0), rect, Color::RED)?;
+
+        graphics::Canvas::draw(canvas, &rect_mesh, DrawParam::default());
+        Ok(())
+    }
+}
+
+struct MainState {
+    car: Car
+}
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let state = MainState{};
+        let pos: Point2<f32> = START_POS;
+        let ego = Car::new(CAR_W, CAR_H, pos);
+        let state = MainState{car: ego};
         Ok(state)
     }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, ct: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        if ctx.keyboard.is_key_pressed(KeyCode::Left) {
+            self.car.pos.x -= 1.0;
+        }
+        else if ctx.keyboard.is_key_pressed(KeyCode::Right) {
+            self.car.pos.x += 1.0;
+        }
+        else if ctx.keyboard.is_key_pressed(KeyCode::Up) {
+            self.car.pos.y -= 1.0;
+        }
+        else if ctx.keyboard.is_key_pressed(KeyCode::Down) {
+            self.car.pos.y += 1.0;
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let canvas = graphics::Canvas::from_frame(
+        let mut canvas = graphics::Canvas::from_frame(
             ctx,
             graphics::Color::from([0.0, 0.5, 0.0, 1.0]),
         );
-
+        self.car.draw(&mut canvas, ctx);
         canvas.finish(ctx)?;
         Ok(())
     }
