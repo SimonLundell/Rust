1eed08ed8975ffca5952e143c2e8725a8ae13ca6
Simon Lundell <simonlundell@hotmail.com>
2023-01-29 16:14:40 +0100

    accelerate/brake

--- racing/src/main.rs
+++ racing/src/main.rs
@@ -10,28 +10,24 @@ const WINDOW_W: f32 = 600.0;
 const WINDOW_H: f32 = 800.0;
 const CAR_W: f32 = 30.0;
 const CAR_H: f32 = 60.0;
-const START_POS: Point2<f32> = Point2{x: WINDOW_W / 2.0, y: WINDOW_H - (2.0 * CAR_H)};
+const START_POS: Point2<f32> = Point2{x: WINDOW_W / 2.0, y: WINDOW_H - (4.0 * CAR_H)};
 const ROAD_CENTER: [Point2<f32>; 2] = [Point2{x: WINDOW_W / 2.0, y: 0.0}, Point2{x: WINDOW_W / 2.0, y: WINDOW_H}];
-const ROAD_LEFT: [Point2<f32>; 2] = [Point2{x: WINDOW_W / 2.0 - CAR_W, y: 0.0}, Point2{x: WINDOW_W / 2.0 - CAR_W, y: WINDOW_H}];
-const ROAD_RIGHT: [Point2<f32>; 2] = [Point2{x: WINDOW_W / 2.0 + CAR_W, y: 0.0}, Point2{x: WINDOW_W / 2.0 + CAR_W, y: WINDOW_H}];
-const DRIVING_SPEED: f32 = 2.0;
 
 struct Road {
     center: [Point2<f32>; 2],
     left: Vec<[Point2<f32>; 2]>,
     right: Vec<[Point2<f32>; 2]>,
     pos: f32,
+    speed: f32,
 }
 
 impl Road {
-    fn new(center: [Point2<f32>; 2], left: Vec<[Point2<f32>; 2]>, right: Vec<[Point2<f32>; 2]>, pos: f32) -> Road {
-        Road{center: center, left: left, right: right, pos: pos}
+    fn new(center: [Point2<f32>; 2], left: Vec<[Point2<f32>; 2]>, right: Vec<[Point2<f32>; 2]>, pos: f32, speed: f32) -> Road {
+        Road{center: center, left: left, right: right, pos: pos, speed: speed}
     }
 
     fn draw(&mut self, canvas: &mut Canvas, ctx: &mut Context) -> GameResult {
         let mb = &mut MeshBuilder::new();
-        self.left = vec![];
-        self.right = vec![];
         self.left = line_builder(10.0, 5.0, -1, self.pos);
         self.right = line_builder(10.0, 5.0, 1, self.pos);
         if self.left.len() == self.right.len() {
@@ -43,7 +39,7 @@ impl Road {
         else {
             println!("Not same amount of road-markings on left/right lane");
         }
-        self.pos += DRIVING_SPEED;
+        self.pos += self.speed;
         if self.pos > 15.0 {
             self.pos = 0.0;
         }
@@ -54,25 +50,35 @@ impl Road {
         graphics::Canvas::draw(canvas, &center_line_mesh, DrawParam::default());
         Ok(())
     }
+
+    fn get_car_speed(&mut self, speed: f32) {
+        self.speed = speed;
+    }
 }
+
 
 struct Car {
     w: f32,
     h: f32,
     pos: Point2<f32>,
+    speed: f32,
 }
 
 impl Car {
-    fn new(w: f32, h: f32, pos: Point2<f32>) -> Car {
-        Car{w: w, h: h, pos: pos}
+    fn new(w: f32, h: f32, pos: Point2<f32>, speed: f32) -> Car {
+        Car{w: w, h: h, pos: pos, speed: speed}
     }
 
     fn draw(&mut self, canvas: &mut Canvas, ctx: &mut Context) -> GameResult {
-        let mut rect = Rect::new(self.pos.x, self.pos.y, self.w, self.h);
+        let rect = Rect::new(self.pos.x, self.pos.y, self.w, self.h);
         // rect.rotate(f32::to_radians(-1.0)); Rotates entire world for some reason
         let rect_mesh = Mesh::new_rectangle(ctx, graphics::DrawMode::stroke(4.0), rect, Color::RED)?;
         graphics::Canvas::draw(canvas, &rect_mesh, DrawParam::default());
         Ok(())
+    }
+
+    pub fn get_speed(self) -> f32 {
+        self.speed
     }
 }
 
@@ -84,8 +90,8 @@ struct MainState {
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let pos: Point2<f32> = START_POS;
-        let road = Road::new(ROAD_CENTER, vec![], vec![], 0.0);
-        let ego = Car::new(CAR_W, CAR_H, pos);
+        let ego = Car::new(CAR_W, CAR_H, pos, 0.0);
+        let road = Road::new(ROAD_CENTER, vec![], vec![], 0.0, 0.0);
         let state = MainState{car: ego, road: road};
         Ok(state)
     }
@@ -100,11 +106,18 @@ impl event::EventHandler<ggez::GameError> for MainState {
             self.car.pos.x += 1.0;
         }
         if ctx.keyboard.is_key_pressed(KeyCode::Up) {
-            self.car.pos.y -= 1.0;
+            self.car.speed += 0.01;
+            if self.car.speed >= 2.5 {
+                self.car.speed = 2.5;
+            }
         }
         if ctx.keyboard.is_key_pressed(KeyCode::Down) {
-            self.car.pos.y += 1.0;
+            self.car.speed -= 0.01;
+            if self.car.speed <= -1.25 {
+                self.car.speed = -1.25;
+            }
         }
+        self.road.get_car_speed(self.car.speed);
         Ok(())
     }
 
